1d336d971e2121d6699266ea1ff2f60322d11c22
MariR14 <marirl14500@gmail.com>
2021-08-17 00:44:30 -0500

    Add groups

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -4,7 +4,6 @@ from django.forms import ModelForm
 from apps.Doctors.models import *
 from apps.user.models import User
 
-
 class FormPersona(ModelForm):
 
     class Meta:
@@ -19,16 +18,18 @@ class FormUser(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = '__all__'
-        widgets = {'password': forms.PasswordInput(),}
+        fields = ['username','email','password','groups']
+        widgets = {'password': forms.PasswordInput(),
+                   'groups': forms.SelectMultiple(
+                       attrs={
+                            'name':'groups',
+                            'id':'groups',
+                            'onChange' : "showInp();"
+                   })}
         exclude =['last_login','is_superuser','first_name','last_name','is_staff','is_active','persona', 'user_permissions', 'date_joined']
 
 
-class FormMP(forms.ModelForm):
-    class Meta:
-        model = PersonaMedico
-        fields = '__all__'
-        exclude =['id_persona']
+
 
 
 
