2e17f77f08e129207cce81580fb46375353332ee
MariR14 <marirl14500@gmail.com>
2021-08-17 23:46:34 -0500

    Add groups

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -9,9 +9,8 @@ class FormPersona(ModelForm):
     class Meta:
         model = Persona
         fields = '__all__'
-        #widgets = {'fecha_nacimiento': forms.DateField()}
-
-
+        widgets = {'fecha_nacimiento': forms.DateInput(format='%Y-%m-%d',
+                                                       attrs={'type':'date'})}
 
 
 class FormUser(forms.ModelForm):
@@ -29,7 +28,20 @@ class FormUser(forms.ModelForm):
         exclude =['last_login','is_superuser','first_name','last_name','is_staff','is_active','persona', 'user_permissions', 'date_joined']
 
 
-
+class FormMP(forms.ModelForm):
+    class Meta:
+        model = PersonaMedico
+        fields = '__all__'
+        widgets ={'cedula_medica': forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Cedula Medica',
+                'id':'cedula',
+                'name':'cedula',
+                'style': 'display: none'
+            }
+        )}
+        exclude =['id_persona']
 
 
 
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login
@@ -17,6 +17,7 @@ class SolicitudCreate(CreateView):
     model = User
     form_class = FormUser
     second_form_class = FormPersona
+    third_form_class = FormMP
     template_name = 'login/registro.html'
     success_url = reverse_lazy('index')
 
@@ -27,7 +28,8 @@ class SolicitudCreate(CreateView):
             context['form'] = self.form_class(self.request.GET)
         if 'form2' not in context:
             context['form2'] = self.second_form_class(self.request.GET)
-
+        if 'form3' not  in context:
+            context['form3'] = self.third_form_class(self.request.GET)
         return context
 
     def post(self, request, *args, **kwargs):
@@ -36,6 +38,7 @@ class SolicitudCreate(CreateView):
 
         form = self.form_class(request.POST)
         form2 = self.second_form_class(request.POST)
+        form3 = self.third_form_class(request.POST)
         # evaluar si son valores son validos para guardarlos
         if form.is_valid() and form2.is_valid():
             pwd = form.cleaned_data['password']
@@ -53,10 +56,7 @@ class SolicitudCreate(CreateView):
             registro.save()
             for g in form.cleaned_data['groups']:
                 registro.groups.add(g) #guardar grupos
-
-
-
-
+            registro.id_persona = form3.save()
 
             return HttpResponseRedirect(self.get_success_url())
         else:
@@ -77,16 +77,15 @@ class login_view(FormView):
     def dispatch(self, request, *args, **kwargs):
 
         if request.user.is_authenticated: #verificar si un usuario se encuentra logueado
-           return HttpResponseRedirect(self.success_url)
+           return redirect(self.success_url)
         return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         login(self.request, form.get_user())
-        return HttpResponseRedirect(self.success_url)
+        return redirect(self.success_url)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data( **kwargs)
-        context['title'] = 'Iniciar Sesion'
         return context
 
 def index(request):
